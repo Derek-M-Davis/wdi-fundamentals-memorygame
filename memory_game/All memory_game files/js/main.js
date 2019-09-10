13bcff121edb5b2a0f7722a7dd8b0453ab8f453e
Derek <derekdavis408@gmail.com>
2019-09-10 11:51:24 -0700

    created more game logic

--- memory_game/All memory_game files/js/main.js
+++ memory_game/All memory_game files/js/main.js
@@ -1,12 +1,20 @@
 console.log("Up and running!");
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-var cardOne = (cards[0]);
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = (cards[2]);
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match");
+    } else {
+        console.log("Sorry, try again");
+    }
+}
+function flipcard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId])
+    checkForMatch();
+}
+flipcard(0)
+flipcard(2)
 if (length.cardsInPlay === 2 && cardsInPlay[0] === cardsInPlay[1] ) {
         alert("You found a match!")}
     else alert("Sorry, try again.");
